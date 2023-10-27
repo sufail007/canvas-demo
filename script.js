c7cbaa02c45e0ed4e7664352457df913bfd76ae8
kaif-staypositive <mohammad@staypositiveit.com>
2023-10-27 15:27:00 +0530

    HTML Canvas: fillRect()

---
+++ script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// fillRect()
+ctx.fillStyle = "red";
+ctx.fillRect(20, 20, 200, 100);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(240, 20, 200, 100);
