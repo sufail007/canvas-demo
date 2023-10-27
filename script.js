f64c892dcb70eaf4252872ef43a87287678ab794
kaif-staypositive <mohammad@staypositiveit.com>
2023-10-27 15:51:47 +0530

    Fill Stroke Text

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ ctx.fillRect(240, 20, 200, 100);
 ctx.lineWidth = 5;
 ctx.strokeStyle = "green";
 ctx.strokeRect(100, 200, 200, 100);
+
+// clearRect()
+// ctx.clearRect(0, 0, 600, 600);
+
+// fillText()
+ctx.font = "16px Arial";
+ctx.fillStyle = "purple";
+ctx.fillText("Hello World!", 500, 50);
+
+// strokeText()
+ctx.lineWidth = 1;
+ctx.strokeStyle = "orange";
+ctx.strokeText("Good Bye", 500, 150);
